78bc05bf3d8117f3c96093ae6544d38cf46ab188
Joshua Ohana <joshua.ohana@gmail.com>
2017-06-28 18:22:04 -0700

    adding basic unit test

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,36 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+
+  let comp:    AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
+      declarations: [ AppComponent ], // declare the test component
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+  });
+
+  it('should display original title', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Tour of Heroes');
+  });
+});
+
+class RouterStub { }
